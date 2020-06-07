882e1dc7c7df0b918f3771100702a1dfb2cd2c5d
malul88 <62894502+malul88@users.noreply.github.com>
2020-06-07 16:53:28 +0300

    Update intMatrix.h

--- intMatrix.h
+++ intMatrix.h
@@ -13,24 +13,22 @@ namespace mtm {
         Dimensions dimensions;
     public:
         intMatrix(Dimensions dims,int init_num = 0);
-        intMatrix(const intMatrix& intmatrix);
+        intMatrix(const intMatrix& matrix1);
         ~intMatrix();
-        intMatrix& operator=(const intMatrix& intmatrix);
-
-
-        intMatrix& identity(Dimensions dims);
-        int height(const intMatrix matrix);
-        int width(const intMatrix matrix);
-        int size(const intMatrix matrix);
-        intMatrix transpose(const intMatrix matrix);
-        intMatrix operator+(const intMatrix matrix) const;
+        intMatrix& operator=(const intMatrix& matrix1);
+        intMatrix identity(int dim);
+        int height();
+        int width();
+        int size();
+        intMatrix transpose();
+        intMatrix operator+(const intMatrix& matrix1) const;
         intMatrix operator-() const;
         intMatrix operator-(const intMatrix matrix) const;
+        intMatrix& operator+=(const int x);
         intMatrix operator+(const int x) const;
-        intMatrix operator+=(const int x) const;
-        std::ostream& operator<<(std::ostream& os) const;
-        intMatrix operator()(const int col, const int row) const;
-        intMatrix& operator()(const int col, const int row);
+        friend std::ostream& operator<<(std::ostream& os,intMatrix& matrix1);
+        const int operator()(const int col, const int row) const;
+        int& operator()(const int col, const int row);
         bool operator<(intMatrix matrix);
         bool operator>(intMatrix matrix);
         bool operator>=(intMatrix matrix);
@@ -58,6 +56,15 @@ namespace mtm {
     bool all(const intMatrix matrix);
     bool any(const intMatrix matrix);
 //TODO symmetric
-
-
+    std::ostream& operator<<(std::ostream& os,intMatrix& matrix1){
+        int squared_dims = matrix1.intMatrix::size();
+        int* mat_to_array = new int[squared_dims];
+        for (int i = 0; i <matrix1.dimensions.getRow() ; ++i) {
+            for (int j = 0; j <matrix1.dimensions.getCol() ; ++j) {
+                mat_to_array[i+j] = matrix1.matrix[i][j];
+            }
+        }
+        return os << printMatrix(mat_to_array,matrix1.dimensions) << std::endl;
+    }
+}
 #endif //INTMATRIX_H
